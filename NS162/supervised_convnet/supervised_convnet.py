5de75302679b958e11144267205439f952f13ce1
anhhuyalex <alexn@minerva.kgi.edu>
2019-06-18 02:45:09 -0700

    change 0/1 labels to +1/-1 labels

--- NS162/supervised_convnet/supervised_convnet.py
+++ NS162/supervised_convnet/supervised_convnet.py
@@ -12,20 +12,29 @@ class SupervisedConvNet(nn.Module):
         super(SupervisedConvNet, self).__init__()
         self.filter_size = filter_size
         self.square_size = square_size
+        self.leakyrelu = torch.nn.LeakyReLU(0.1)
         self.conv2d = nn.Conv2d(1, 1, filter_size, padding=0, stride = filter_size)  
-        self.linear1 = nn.Linear(filter_size ** 2, 100)
-        self.linear2 = nn.Linear(100, 1)
+        self.linear1 = nn.Linear(filter_size ** 2, 1)
+        # self.linear2 = nn.Linear(100, 1)
         
+
     def forward(self, x):
         # add hidden layers with relu activation function
-        layer1 = F.relu(self.conv2d(x))
+        layer1 = torch.tanh(self.conv2d(x))
         reshape = layer1.view(-1, 1, self.square_size)
-        linear = self.linear(reshape)
-        layer2 = torch.sigmoid(linear)
+        layer2 = torch.tanh(self.linear1(reshape))
+        # layer3 = torch.tanh(self.linear2(layer2))
+        # layer3 = torch.clamp(layer3, 0, 1)
         # for row in x:
         #     print("row", row)
         #     for el in row[0]:
         #         print("el", el)
         # x = torch.tanh(self.decoder(x))
-        return conv2, layer1, reshape, linear, layer2
+        return reshape, layer2#, layer3
 
+def adjust_learning_rate(optimizer, epoch, lr):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = lr * (0.1 ** (epoch // 200))
+    print ("learning rate", lr)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
--- NS162/supervised_convnet/train.py
+++ NS162/supervised_convnet/train.py
@@ -11,9 +11,9 @@ from sklearn.model_selection import train_test_split
 
 
 uncorrelated_data = np.load("../ising81x81_temp1_uncorrelated9x9.npy")
-correlated_data = np.load("../ising81x81_temp1.npy")[:,:9,:9]
+correlated_data = np.load("../ising81x81_temp1.npy")[:10000,:9,:9]
 data = np.vstack((uncorrelated_data, correlated_data))
-label = np.hstack((np.zeros(10000), np.ones(10000)))
+label = np.hstack((-np.ones(10000), np.ones(10000)))
 X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.33, random_state=42)
 
 
@@ -30,29 +30,41 @@ class IsingDataset(Dataset):
         return len(self.X)
    
 
-isingdataset = IsingDataset(X_train[:100], y_train[:100])
-
-model = supervised_convnet.SupervisedConvNet(filter_size = 3, square_size = 9)
-
-# specify loss function
-criterion = nn.BCELoss()
-
-# specify loss function
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+isingdataset = IsingDataset(X_train[:200], y_train[:200])
+print(isingdataset.y)
+# raise ValueError
 
 # Create training and test dataloaders
 num_workers = 0
 # how many samples per batch to load
-batch_size = 5
+batch_size = 1
 # number of epochs to train the model
-n_epochs = 500
+n_epochs = 100
+# learning rate
+lr = 0.01
+# adjust learning rate?
+adjust_learning_rate = False
+
+# specify loss function
+criterion = nn.MSELoss()
+
+# build model
+model = supervised_convnet.SupervisedConvNet(filter_size = 3, square_size = 9)
+
+# specify optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
 
 # prepare data loaders
 train_loader = torch.utils.data.DataLoader(isingdataset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
 
 for epoch in range(1, n_epochs+1):
     # monitor training loss
-    train_loss = 0.0
+    accuracy = 0.0
+
+    # adjust learning rate
+    if adjust_learning_rate == True:
+        supervised_convnet.adjust_learning_rate(optimizer, epoch, lr)
 
     ###################
     # train the model #
@@ -61,43 +73,78 @@ for epoch in range(1, n_epochs+1):
         data = data.unsqueeze(1).type('torch.FloatTensor')
         target = target.type('torch.FloatTensor')
         optimizer.zero_grad()
-        output = model(data)[4].view(-1)
-        loss = criterion(output, target)
+        output = model(data)[-1].view(-1)
+        loss = criterion(output, target) 
         # add regularization
         # for param in model.parameters():
-        #     loss += (param.view(-1)).sum()**2  
+        #     loss += ((param)**2).sum()/200
         loss.backward()
         optimizer.step()
 
         # update running training loss
-        train_loss += loss.item() * batch_size
+        accuracy += (torch.sign(output) == target).item()
     
     # print avg training statistics 
-    train_loss = train_loss/len(train_loader)
+    # train_loss = train_loss/len(train_loader)
     if epoch % 1 == 0:
-        print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+        print('Epoch: {} \tTraining Loss: {}'.format(
             epoch, 
-            train_loss
+            accuracy
             ))
 #         print("data", data[:10])
         # print("output", (output)[:10])
         # print("target", (target)[:10])
-        for name, param in model.named_parameters():
-            if param.requires_grad:
-                print (name, param.data)
+        # for name, param in model.named_parameters():
+        #     if param.requires_grad:
+        #         print (name, param.data)
 
 for batch_idx, (data, target) in enumerate(train_loader):
     data = data.unsqueeze(1).type('torch.FloatTensor')#[0].unsqueeze(1)
-    print("data", data)
+    # print("data", data)
     target = target.type('torch.FloatTensor')
     optimizer.zero_grad()
-    output = model(data)[-1].view(-1)
-    print("output", output)
-    print("target", target)
+    output = [i.view(-1) for i in model(data)]
+    print("data", data[:10])
+    print("output", (output[:10]))
+    print("target", target[:10])
+    v = torch.tensor([[[[-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.]]]])
+    print("correlated model(v)", model(v))
+    v = torch.tensor([[[[ 1.,  1.,  1.,  1.,  1.,  1., -1., -1., -1.],
+          [ 1.,  1.,  1.,  1.,  1.,  1., -1., -1., -1.],
+          [ 1.,  1.,  1.,  1.,  1.,  1., -1., -1., -1.],
+          [ 1.,  1.,  1.,  1.,  1.,  1., -1., -1., -1.],
+          [ 1.,  1.,  1.,  1.,  1.,  1., -1., -1., -1.],
+          [ 1.,  1.,  1.,  1.,  1.,  1., -1., -1., -1.],
+          [-1., -1., -1.,  1.,  1.,  1., -1., -1., -1.],
+          [-1., -1., -1.,  1.,  1.,  1., -1., -1., -1.],
+          [-1., -1., -1.,  1.,  1.,  1., -1., -1., -1.]]]])
+    print("uncorrelated model(v)", model(v))
     # loss = criterion(output, target[0])
     # print("loss.data", loss.data)
     # loss.backward()
-    # for name, param in model.named_parameters():
-    #     if param.requires_grad:
-    #         print (name, param.grad)
+
+v = torch.tensor([[[[-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1.,  1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.],
+          [-1., -1., -1., -1., -1., -1., -1., -1., -1.]]]])
+print("negative", model(v))
+print("positive", model(-v))
+for name, param in model.named_parameters():
+    if param.requires_grad:
+        print (name, param.data)
+
+torch.save(model.state_dict(), "9x9->3x3.pt")
     # optimizer.step()
